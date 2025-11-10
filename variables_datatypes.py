dfd02a6213c16bf89f0f17676829c0be0454f2ec
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-11-11 02:10:47 +0600

    variables and datatypes

---
+++ variables_datatypes.py
@@ -0,0 +1,28 @@
+# x = 1           #int
+# y = 2.5         #float
+# name = 'Pabitra' #string
+# check_reality = True    #bool
+
+# print(name, x, y, check_reality)
+
+# multiple assign
+x, y, name, r = (5, 4.234, 'kumar', False)
+print(x, y, r, name)
+print(type(x))
+print(type(r))
+print(type(name))
+
+
+# casting
+x = str(x)
+
+print(x)
+print(type(x))
+
+x = float(x)
+print(x)
+print(type(x))
+
+y = float(y)
+print(y)
+print(type(y))
