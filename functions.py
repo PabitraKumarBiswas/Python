ad205f12af5d56a70b3f526b77f371204fd3ea21
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-11-13 17:02:26 +0600

    func's.py

---
+++ functions.py
@@ -0,0 +1,10 @@
+# creat function 
+# def say_hello(name):
+#     print('hello ' + name)
+
+# say_hello('Pabitra Kumar')
+
+def say_hello(name = 'Pabitra Kumar'):
+    print('hello ' + name)
+
+say_hello('Priya Sarker')
