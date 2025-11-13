c7cfda6925a971d8e901cefb2b2ba681493c9f64
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-11-13 18:16:22 +0600

    func's 2nd commit

--- functions.py
+++ functions.py
@@ -6,5 +6,24 @@
 
 def say_hello(name = 'Pabitra Kumar'):
     print('hello ' + name)
-
+"""'say_hello' functions override the name 'Pabitra Kumar'. """
 say_hello('Priya Sarker')
+
+
+# return value 
+def get_sum(num1, num2):
+    total = num1 + num2
+    return total
+print(get_sum(13, 7))
+
+num_sum = get_sum(21, 13)
+print(num_sum)
+
+def add_one_to_num(number):
+    # number = number + 1
+    number += 1 #short form
+    return number
+# print(add_one_to_num(5))
+number = 10
+new_number = add_one_to_num(number)
+print(new_number)
