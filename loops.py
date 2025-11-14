5474f9c0e99ef9851210457276fb34896f68f602
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-11-15 03:04:29 +0600

    Loops

---
+++ loops.py
@@ -0,0 +1,27 @@
+people = ['Pabitra', 'Pulak', 'Piyush', 'Parimal Chandra Biswas', 'Nilima Rani Biswas']
+
+#simple for loop
+# for person in people:
+#     print('Current Person: ', person)
+
+# #Break out
+# for person in people:
+#     print(f'person name: ', person)
+#     if person == 'Piyush':
+#         break
+# # continue 
+# for person in people:
+#     if person == 'Pulak':
+#         continue
+#     print('names: ', person)
+
+# # range 
+# for i in range(0, 15):
+#     print('Person name: ', i)
+
+
+# count
+count = 0
+while count <= 1000:
+    print(f'Count: {count}')
+    count += 1
